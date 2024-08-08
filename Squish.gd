f1988a28f733045b494fce589d9984367512ced9
Dylan James <alazyartist@gmail.com>
2024-08-08 03:38:50 -0600

    is on floor and bouncing

--- Squish.gd
+++ Squish.gd
@@ -11,10 +11,19 @@ func _process(delta):
 	get_input()
 	pass
 
+var speed = 5
 func get_input():
-	var speed = 5
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	$Head.apply_impulse(input_direction * 2*speed,Vector2(0,0))
-	
-	if Input.is_action_just_pressed('ui_accept'):
+	if not Input.is_action_pressed("up"):
+		$Head.apply_impulse(input_direction * 2 * speed,Vector2(0,0))
+	if Input.is_action_pressed("up") and $Base/onFloor.is_colliding():
+		$Head.apply_impulse(input_direction * 2 * speed,Vector2(0,0))
+		
+	if Input.is_action_just_pressed('ui_accept') and $Base/onFloor.is_colliding():
 		$Base.apply_impulse(Vector2(0,-16*4))
+
+
+func _squish_bounce():
+	$Head.apply_central_impulse(Vector2(0,-100))
+	
+	pass # Replace with function body.
